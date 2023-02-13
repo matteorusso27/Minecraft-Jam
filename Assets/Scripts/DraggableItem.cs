ffc99339fad9597de8130c57842e2d02cb19924c
matteorusso27 <matteo2795@gmail.com>
2023-02-13 22:42:23 +0100

    first crafting system

---
+++ Assets/Scripts/DraggableItem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    private Image img;
+    [HideInInspector] public Transform parentAfterDrag;
+
+    private void Awake()
+    {
+        img = GetComponent<Image>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        img.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
+        img.raycastTarget = true;
+    }
+ 
+}
